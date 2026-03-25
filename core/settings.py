281ce559481a78b236418be9647932d6efabd957
Yusuf-Babagana <yusufmuhdkk@gmail.com>
2026-03-25 04:23:40 -0700

    Add requirements.txt for production deployment

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-d0yp70vgtbv69vv$nczea(ujnbjkc1h_*rt$#cw!1_-iz%6y)h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.254', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['ticbackend.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
