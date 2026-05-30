0cbf69e6496d88aeb86dcfc400e0d8735906f871
Yusuf-Babagana <yusufmuhdkk@gmail.com>
2026-05-30 02:10:25 -0700

    Updated VTU services and API logic

--- core/urls.py
+++ core/urls.py
@@ -17,14 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.http import HttpResponseRedirect
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 from dashboard.views import AdminDashboardView
 
 urlpatterns = [
-    path('', lambda r: HttpResponseRedirect('/dashboard/'), name='root'),
+    path('', RedirectView.as_view(url=reverse_lazy('admin_dashboard'), permanent=False), name='root'),
     path('dashboard/', AdminDashboardView.as_view(), name='admin_dashboard'),
     path('admin/', admin.site.urls),
     path('dashboard/logout/', LogoutView.as_view(next_page='/dashboard/'), name='dashboard_logout'),
